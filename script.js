2e4c09f86cd7848ae2daedb7eaa9552334cd91f9
KayaSake <mikehferguson@gmail.com>
2023-02-15 18:26:04 -0300

    Commit to be safe

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
+
 function getComputerChoice() {
-    a = Math.floor(Math.random() * 3);
-    if (a === 0) {return"rock"}
-    else if(a === 1) {return"paper"}
+    computerChoice = Math.floor(Math.random() * 3);
+    if (computerChoice === 0) {return"rock"}
+    else if(computerChoice === 1) {return"paper"}
     else {return"scissors"};
 }
 
+function getPlayerChoice() {
+    let playerChoice = prompt ("Choose you weapon");
+    return (playerChoice);
+}
+
+function playRound(playerChoice, computerChoice) {
+   
+    let playerChoice = getPlayerChoice();
+    let computerChoice = getComputerChoice();
+    if ((playerChoice == rock) & (computerChoice == scissors)
+    || (playerChoice == paper) & (computerChoice == rock)
+    || (playerChoice == scissors) & (computerChoice == paper))
+    {return(`YOU WIN!!! You chose ${playerChoice}, Computer chose ${computerChoice}`)}
+    else if (playerChoice = computerChoice) {
+        return(`It's a tie! You both chose ${playerChoice}`)
+    }else {return(`YOU Lose!!! You chose ${playerChoice}, Computer chose ${computerChoice}`)}
+}
